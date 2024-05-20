fa7d1401cfc637187af18390a0968724a84df3c6
Jasiel <jasieloficial@hotmail.com>
2024-05-20 09:11:15 -0300

    feat: Implementado cors

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,22 @@ import { routes } from "./routes";
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [process.env.FRONTEND, "http://localhost:5173"];
+
+const corsOptions: cors.CorsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin!) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(routes)
+app.use(routes);
 
 app.listen(process.env.PORT, () => {
   console.log(`🚀 Server running`);
 });
-
